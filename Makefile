edf86dd6eb005c6156f59c8dc4f0fa91ce53b496
zphricz <zphricz@vt.edu>
2015-03-17 20:43:02 -0400

    - premature commit

--- Makefile
+++ Makefile
@@ -1,6 +1,28 @@
-CXXFLAGS=-Ofast -std=c++11 -Werror -lpthread
+CXXFLAGS = -std=c++11 -Ofast -Wall -Werror
+LDFLAGS = -lSDL2
+OS = $(shell uname -s)
+SRC = $(wildcard *.cpp)
+OBJECTS = $(patsubst %.cpp, %.o, $(SRC))
+HEADERS = $(patsubst %.cpp, %.h, $(SRC))
+DEPS = $(patsubst %.cpp, %.d, $(SRC))
+ELFNAME = sleepsort
 
-sleepsort: sleepsort.cpp
+ifeq ($(OS), Darwin)
+	CXX = g++-4.9
+endif
+ifeq ($(OS), Linux)
+	CXX = g++
+endif
+
+all: $(ELFNAME)
+
+$(ELFNAME): $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o$@ $^
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c -MMD -MP $< -o $@
+
+-include $(DEPS)
 
 clean:
-	rm -f sleepsort
+	rm -f *.d *.o $(ELFNAME)
--- sleepsort.cpp
+++ sleepsort.cpp
@@ -7,89 +7,88 @@
 using namespace std;
 
 void do_sleep(unsigned int val, bool &run, condition_variable &sig,
-              unsigned int *& loc, mutex &m) {
-    /* Wait until all sleepers are spawned */
-    if (!run) {
-        unique_lock<mutex> lock(m);
-        while (!run) {
-            sig.wait(lock);
-        }
+              unsigned int *&loc, mutex &m) {
+  /* Wait until all sleepers are spawned */
+  if (!run) {
+    unique_lock<mutex> lock(m);
+    while (!run) {
+      sig.wait(lock);
     }
-    usleep(val * 10000);
+  }
+  usleep(val * 10000);
 
-    /* Place value at destination */
-    unsigned int * destination;
-    {
-        lock_guard<mutex> lock(m);
-        destination = loc++;
-    }
-    *destination = val;
+  /* Place value at destination */
+  unsigned int *destination;
+  {
+    lock_guard<mutex> lock(m);
+    destination = loc++;
+  }
+  *destination = val;
 }
 
 void sleep_sort(unsigned int *arr, int size) {
-    mutex m;
-    condition_variable sig;
-    bool run = false;
-    vector<thread> threads;
+  mutex m;
+  condition_variable sig;
+  bool run = false;
+  vector<thread> threads;
 
-    /* Spawn the sleepers */
-    for (int i = 0; i < size; ++i) {
-        threads.push_back(
-            thread(do_sleep, arr[i], ref(run), ref(sig), ref(arr), ref(m)));
-    }
-    {
-        lock_guard<mutex> lock(m);
-        run = true;
-        sig.notify_all();
-    }
+  /* Spawn the sleepers */
+  for (int i = 0; i < size; ++i) {
+    threads.push_back(
+        thread(do_sleep, arr[i], ref(run), ref(sig), ref(arr), ref(m)));
+  }
+  {
+    lock_guard<mutex> lock(m);
+    run = true;
+    sig.notify_all();
+  }
 
-    /* Wait for the sleepers */
-    for (auto &t : threads) {
-        t.join();
-    }
+  /* Wait for the sleepers */
+  for (auto &t : threads) {
+    t.join();
+  }
 }
 
-int check_sorted(unsigned int * arr, int size) {
-    int cnt = 0;
-    for (int i = 0; i < size - 1; ++i) {
-        if (arr[i] > arr[i + 1]) {
-            cout << "FAILURE ON: [" << arr[i] << ", " << arr[i + 1] << ']' << endl;
-            cnt++;
-        }
+int check_sorted(unsigned int *arr, int size) {
+  int cnt = 0;
+  for (int i = 0; i < size - 1; ++i) {
+    if (arr[i] > arr[i + 1]) {
+      cout << "FAILURE ON: [" << arr[i] << ", " << arr[i + 1] << ']' << endl;
+      cnt++;
     }
-    return cnt;
+  }
+  return cnt;
 }
 
 int main(int argc, char *argv[]) {
-    
-    if (argc != 2) {
-        cout << "USAGE: " << argv[0] << " <array size>" << endl;
-        return 1;
-    }
-    int size = atoi(argv[1]);
-    unsigned int *arr = new unsigned int[size];
 
-    srand(time(NULL));
-    for (int i = 0; i < size; ++i) {
-        arr[i] = rand() % 100;
-    }
+  if (argc != 2) {
+    cout << "USAGE: " << argv[0] << " <array size>" << endl;
+    return 1;
+  }
+  int size = atoi(argv[1]);
+  unsigned int *arr = new unsigned int[size];
 
-    sleep_sort(arr, size);
+  srand(time(NULL));
+  for (int i = 0; i < size; ++i) {
+    arr[i] = rand() % 100;
+  }
 
-    /* Pretty print it */
-    cout << "[";
-    for (int i = 0; i < size - 1; ++i) {
-        cout << arr[i] << ", ";
-    }
-    cout << arr[size - 1] << "]" << endl;
+  sleep_sort(arr, size);
 
-    int num_failures = check_sorted(arr, size);
-    if (num_failures == 0) {
-        cout << "SUCCESS" << endl;
-    }
-    else {
-        cout << "There were " << num_failures << " failures" << endl;
-    }
+  /* Pretty print it */
+  cout << "[";
+  for (int i = 0; i < size - 1; ++i) {
+    cout << arr[i] << ", ";
+  }
+  cout << arr[size - 1] << "]" << endl;
 
-    return 0;
+  int num_failures = check_sorted(arr, size);
+  if (num_failures == 0) {
+    cout << "SUCCESS" << endl;
+  } else {
+    cout << "There were " << num_failures << " failures" << endl;
+  }
+
+  return 0;
 }
